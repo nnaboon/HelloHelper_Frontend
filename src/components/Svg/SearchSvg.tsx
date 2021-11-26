40c5587097190ac6c959b29f669093b038f98987
nnaboon <srisawasdina@gmail.com>
2021-11-26 19:49:57 +0700

    feat: add svg icon

---
+++ src/components/Svg/SearchSvg.tsx
@@ -0,0 +1,27 @@
+export const SearchSvg = (props) => {
+  return (
+    <svg
+      width="25"
+      height="25"
+      viewBox="0 0 37 37"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M16.9583 29.2917C23.7698 29.2917 29.2917 23.7698 29.2917 16.9583C29.2917 10.1468 23.7698 4.625 16.9583 4.625C10.1468 4.625 4.625 10.1468 4.625 16.9583C4.625 23.7698 10.1468 29.2917 16.9583 29.2917Z"
+        stroke="black"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M32.3747 32.3747L25.6685 25.6685"
+        stroke="black"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
