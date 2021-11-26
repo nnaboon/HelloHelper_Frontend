b63fb8adffd4a307df90efe11562639ee3b03bf2
nnaboon <srisawasdina@gmail.com>
2021-11-26 14:14:32 +0700

    feat: add community mock data

---
+++ src/data/community.ts
@@ -0,0 +1,52 @@
+export const COMMUNITY_MAPPER = [
+  {
+    id: 'zxcvb234',
+    name: 'ภาคคอม มก',
+    code: 'computerDepartmentKU',
+    admin: [
+      {
+        id: 'abcde',
+        name: 'กระทิง สีแดง'
+      }
+    ],
+    member: [
+      {
+        id: 'abcdf',
+        name: 'กระทิงสีทอง'
+      },
+      {
+        id: 'abcdg',
+        name: 'กระทิง สีน้ำเงิน'
+      },
+      {
+        id: 'abcdh',
+        name: 'Bull Yellow'
+      }
+    ]
+  },
+  {
+    id: 'zxcvb345',
+    name: 'ภาคคอม จุฬา',
+    code: 'computerDepartmentKU',
+    admin: [
+      {
+        id: 'abcda',
+        name: 'กระทิง สีเงิน'
+      }
+    ],
+    member: [
+      {
+        id: 'abcdf',
+        name: 'กระทิงสีทอง'
+      },
+      {
+        id: 'abcdg',
+        name: 'กระทิง สีน้ำเงิน'
+      },
+      {
+        id: 'abcde',
+        name: 'กระทิง สีแดง'
+      }
+    ]
+  }
+];
