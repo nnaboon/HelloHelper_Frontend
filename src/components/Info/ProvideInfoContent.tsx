89636b08f8eb28a4544abf8c8e5bcef79055a47d
nnaboon <srisawasdina@gmail.com>
2021-11-26 19:53:08 +0700

    feat: add info content

---
+++ src/components/Info/ProvideInfoContent.tsx
@@ -0,0 +1,166 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import React from 'react';
+import styled from '@emotion/styled';
+import { css, jsx } from '@emotion/react';
+import { useLocation, useHistory } from 'react-router-dom';
+import { WrapperContainer } from 'components/Wrapper/WrapperContainer';
+import Flex from 'components/Flex/Flex';
+import { Text } from 'components/Text';
+import { PrimaryButton, SecondaryButton } from 'components/Button/Button';
+import { HelpMenu } from 'components/Menu/const';
+import { UserSvg } from 'components/Svg/UserSvg';
+
+const ProvideImage = styled.img`
+  width: 420px;
+  height: 510px;
+  background: yellow;
+  margin-bottom: 20px;
+`;
+
+const ProvideCategoryButton = styled(PrimaryButton)`
+  width: max-content;
+  min-width: 140px;
+  padding: 10px 15px;
+  height: 40px;
+  margin: 10px 8px 10px 0px;
+`;
+
+const ProvideHashtagButton = styled(SecondaryButton)`
+  width: max-content;
+  min-width: 80px;
+  padding: 10px 15px;
+  height: 40px;
+  margin: 10px 8px 10px 0px;
+`;
+
+const ProvideInfoContainer = styled.div`
+  display: grid;
+  grid-template-columns: 180px 400px;
+  grid-gap: 40px;
+  margin-bottom: 60px;
+`;
+
+const ProvideDetail = styled.div`
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 28px;
+  color: #000000;
+  min-width: 200px;
+`;
+
+const ProvideTitle = styled.div`
+  font-size: 14px;
+  line-height: 26px;
+  color: #cacaca;
+  min-width: 90px;
+  max-width: 150px;
+`;
+
+export const ProvideInfoContent = ({ data }: any) => {
+  const history = useHistory();
+  const { pathname, state } = useLocation();
+  const query = pathname.split('/')[2];
+  const currentMenu = ((state as any)?.type || HelpMenu.PROVIDE) as HelpMenu;
+
+  return (
+    <React.Fragment>
+      {data
+        .filter(({ id }) => id === query)
+        .map(
+          ({
+            id,
+            name,
+            imageUrl,
+            title,
+            location,
+            message,
+            helpSum,
+            serviceCharge,
+            category,
+            hashtag,
+            payment
+          }) => (
+            <WrapperContainer key={id}>
+              <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+                <Flex
+                  direction="column"
+                  justify="flex-start"
+                  itemAlign="flex-start"
+                  style={{ width: 'unset', position: 'relative', left: '6%' }}
+                >
+                  <ProvideImage />
+                  <Flex
+                    css={css`
+                      width: 600px;
+                      flex-wrap: wrap;
+                    `}
+                  >
+                    {category.map((items) => (
+                      <ProvideCategoryButton
+                        onClick={() => {
+                          history.push({
+                            pathname: `/${items}`
+                          });
+                        }}
+                      >
+                        {items}
+                      </ProvideCategoryButton>
+                    ))}
+                  </Flex>
+                  <Flex
+                    css={css`
+                      width: 600px;
+                      flex-wrap: wrap;
+                    `}
+                  >
+                    {hashtag.map((items) => (
+                      <ProvideHashtagButton
+                        onClick={() => {
+                          history.push({
+                            pathname: `/search`,
+                            search: `?keyword=${items}`
+                          });
+                        }}
+                      >
+                        #{items}
+                      </ProvideHashtagButton>
+                    ))}
+                  </Flex>
+                </Flex>
+                <Flex
+                  direction="column"
+                  marginTop="80px"
+                  style={{ width: 'unset' }}
+                >
+                  <ProvideInfoContainer>
+                    <ProvideTitle>ชื่อ</ProvideTitle>
+                    <ProvideDetail>{title}</ProvideDetail>
+                    <ProvideTitle>สถานที่ให้ความข่วยเหลือ</ProvideTitle>
+                    <ProvideDetail>{location}</ProvideDetail>
+                    <ProvideTitle>ยอดการช่วยเหลือ</ProvideTitle>
+                    <ProvideDetail>{helpSum}</ProvideDetail>
+                    <ProvideTitle>อัตราค่าบริการ</ProvideTitle>
+                    <ProvideDetail>{serviceCharge}</ProvideDetail>
+                    <ProvideTitle>ช่องทางการชำระเงิน</ProvideTitle>
+                    <ProvideDetail>{payment}</ProvideDetail>
+                    <ProvideTitle>คำอธิบาย</ProvideTitle>
+                    <ProvideDetail>{message}</ProvideDetail>
+                  </ProvideInfoContainer>
+                  <Flex>
+                    <SecondaryButton style={{ width: '35%', height: '45px' }}>
+                      <UserSvg />
+                      <div>โปรไฟล์</div>
+                    </SecondaryButton>
+                    <PrimaryButton style={{ width: '35%', height: '45px' }}>
+                      สนใจขอความช่วยเหลือ
+                    </PrimaryButton>
+                  </Flex>
+                </Flex>
+              </div>
+            </WrapperContainer>
+          )
+        )}
+    </React.Fragment>
+  );
+};
---
+++ src/components/Info/RequestInfoContent.tsx
@@ -0,0 +1,193 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import React from 'react';
+import styled from '@emotion/styled';
+import { css, jsx } from '@emotion/react';
+import { useLocation, useHistory } from 'react-router-dom';
+import { WrapperContainer } from 'components/Wrapper/WrapperContainer';
+import { Divider } from 'components/Divider/Divider';
+import Flex from 'components/Flex/Flex';
+import { Text } from 'components/Text';
+import { PrimaryButton, SecondaryButton } from 'components/Button/Button';
+import { HelperListCard } from 'components/Card/HelperListCard';
+import { SmallSuggestRequestCard } from 'components/Card/SmallSuggestRequestCard';
+import { HelpMenu } from 'components/Menu/const';
+
+const RequestImage = styled.img`
+  width: 420px;
+  height: 510px;
+  background: yellow;
+  margin-bottom: 20px;
+`;
+
+const RequestCategoryButton = styled(PrimaryButton)`
+  width: max-content;
+  min-width: 140px;
+  padding: 10px 15px;
+  height: 40px;
+  margin: 10px 8px 10px 0px;
+`;
+
+const RequestHashtagButton = styled(SecondaryButton)`
+  width: max-content;
+  min-width: 80px;
+  padding: 10px 15px;
+  height: 40px;
+  margin: 10px 8px 10px 0px;
+`;
+
+const RequestInfoContainer = styled.div`
+  display: grid;
+  grid-template-columns: 180px 400px;
+  grid-gap: 40px;
+  margin-bottom: 60px;
+`;
+
+const RequestDetail = styled.div`
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 28px;
+  color: #000000;
+  min-width: 200px;
+`;
+
+const RequestTitle = styled.div`
+  font-size: 14px;
+  line-height: 26px;
+  color: #cacaca;
+  min-width: 90px;
+  max-width: 150px;
+`;
+
+export const RequestInfoContent = ({ data }: any) => {
+  const history = useHistory();
+  const { pathname, state } = useLocation();
+  const query = pathname.split('/')[2];
+  const currentMenu = ((state as any)?.type || HelpMenu.PROVIDE) as HelpMenu;
+
+  return (
+    <React.Fragment>
+      {data
+        .filter(({ id }) => id === query)
+        .map(
+          ({
+            id,
+            name,
+            imageUrl,
+            title,
+            location,
+            maxPrice,
+            message,
+            maxServiceCharge,
+            category,
+            hashtag,
+            amount,
+            payment,
+            helper,
+            rank
+          }) => (
+            <WrapperContainer key={id}>
+              <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+                <Flex
+                  direction="column"
+                  justify="flex-start"
+                  itemAlign="flex-start"
+                  style={{ width: 'unset', position: 'relative', left: '6%' }}
+                >
+                  <RequestImage />
+                  <Flex
+                    css={css`
+                      width: 600px;
+                      flex-wrap: wrap;
+                    `}
+                  >
+                    {category.map((items) => (
+                      <RequestCategoryButton
+                        onClick={() => {
+                          history.push({
+                            pathname: `/${items}`
+                          });
+                        }}
+                      >
+                        {items}
+                      </RequestCategoryButton>
+                    ))}
+                  </Flex>
+                  <Flex
+                    css={css`
+                      width: 600px;
+                      flex-wrap: wrap;
+                    `}
+                  >
+                    {hashtag.map((items) => (
+                      <RequestHashtagButton
+                        onClick={() => {
+                          history.push({
+                            pathname: `/search`,
+                            search: `?keyword=${items}`
+                          });
+                        }}
+                      >
+                        #{items}
+                      </RequestHashtagButton>
+                    ))}
+                  </Flex>
+                </Flex>
+                <Flex
+                  direction="column"
+                  marginTop="80px"
+                  style={{ width: 'unset' }}
+                >
+                  <RequestInfoContainer>
+                    <RequestTitle>ชื่อ</RequestTitle>
+                    <RequestDetail>{title}</RequestDetail>
+                    <RequestTitle>สถานที่ให้ความข่วยเหลือ</RequestTitle>
+                    <RequestDetail>{location}</RequestDetail>
+                    <React.Fragment>
+                      <RequestTitle>จำนวน</RequestTitle>
+                      <RequestDetail>{amount}</RequestDetail>
+                      <RequestTitle>ราคาสินค้าสูงสุด</RequestTitle>
+                      <RequestDetail>{maxPrice}</RequestDetail>
+                    </React.Fragment>
+                    <RequestTitle>อัตราค่าบริการสูงสุด</RequestTitle>
+                    <RequestDetail>{maxServiceCharge}</RequestDetail>
+                    <RequestTitle>ช่องทางการชำระเงิน</RequestTitle>
+                    <RequestDetail>{payment}</RequestDetail>
+                    <RequestTitle>คำอธิบาย</RequestTitle>
+                    <RequestDetail>{message}</RequestDetail>
+                  </RequestInfoContainer>
+                  <PrimaryButton style={{ width: '100%' }}>
+                    สนใจให้ความช่วยเหลือ
+                  </PrimaryButton>
+                </Flex>
+              </div>
+              <Divider />
+              <Text fontSize="24px" fontWeight={400}>
+                รายชื่อผู้ต้องการช่วยเหลือ
+              </Text>
+              <Flex justify="flex-start" itemAlign="flex-start">
+                <div
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'flex-start',
+                    alignItems: 'flex-start',
+                    width: '100%',
+                    marginTop: '40px'
+                  }}
+                >
+                  {helper.map(({ id, name, imageUrl }) => (
+                    <HelperListCard id={id} name={name} imageUrl={imageUrl} />
+                  ))}
+                </div>
+                <Flex direction="column" itemAlign="flex-end">
+                  <SmallSuggestRequestCard />
+                  <SmallSuggestRequestCard />
+                </Flex>
+              </Flex>
+            </WrapperContainer>
+          )
+        )}
+    </React.Fragment>
+  );
+};
