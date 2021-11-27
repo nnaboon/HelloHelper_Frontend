3d4ad78cdb24401453214616315f0bb92d7fd9ef
nnaboon <srisawasdina@gmail.com>
2021-11-27 17:05:19 +0700

    feat: add const

--- src/features/login/const.ts
+++ src/features/login/const.ts
@@ -1,19 +1,19 @@
 export enum RegisterStep {
-    EMAIL_AND_PASSWORD = 'email-and-password',
-    USERNAME = 'username',
-    LOCATION = 'location',
-    ABILITY = 'ability'
+  EMAIL_AND_PASSWORD = 'email-and-password',
+  USERNAME = 'username',
+  LOCATION = 'location',
+  ABILITY = 'ability'
 }
 
 export enum LoginStep {
-    LOGIN = 'login',
-    REGISTER = 'register'
+  LOGIN = 'login',
+  REGISTER = 'register'
 }
 
 export type UserCreateBody = {
-    email: string;
-    password: string;
-    username: string;
-    location: string;
-    ability: string[];
+  email: string;
+  password: string;
+  username: string;
+  location: string;
+  ability: string[];
 };
