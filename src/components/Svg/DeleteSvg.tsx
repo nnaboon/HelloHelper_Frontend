b02fb7bee28d5d0734c96e4475ee26b9ffd99e8a
nnaboon <srisawasdina@gmail.com>
2021-11-27 17:06:03 +0700

    feat: add svg

---
+++ src/components/Svg/DeleteSvg.tsx
@@ -0,0 +1,41 @@
+export const DeleteSvg = (props) => {
+  return (
+    <svg
+      width="25"
+      height="25"
+      viewBox="0 0 25 25"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M3.95215 6.95801H5.95215H21.9521"
+        stroke="black"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M8.95215 6.95801V4.95801C8.95215 4.42757 9.16286 3.91887 9.53794 3.54379C9.91301 3.16872 10.4217 2.95801 10.9521 2.95801H14.9521C15.4826 2.95801 15.9913 3.16872 16.3664 3.54379C16.7414 3.91887 16.9521 4.42757 16.9521 4.95801V6.95801M19.9521 6.95801V20.958C19.9521 21.4884 19.7414 21.9971 19.3664 22.3722C18.9913 22.7473 18.4826 22.958 17.9521 22.958H7.95215C7.42172 22.958 6.91301 22.7473 6.53793 22.3722C6.16286 21.9971 5.95215 21.4884 5.95215 20.958V6.95801H19.9521Z"
+        stroke="black"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M10.9521 11.958V17.958"
+        stroke="black"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M14.9521 11.958V17.958"
+        stroke="black"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
