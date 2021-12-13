c5f3db3977ea4298c8b270d032c16802a7a9b2e4
nnaboon <srisawasdina@gmail.com>
2021-12-13 22:52:30 +0700

    feat: delete unnecessary code

--- src/pages/Infopage.tsx
+++ src/pages/Infopage.tsx
@@ -1,66 +1,13 @@
-/** @jsxRuntime classic */
-/** @jsx jsx */
 import React from 'react';
-import styled from '@emotion/styled';
-import { css, jsx } from '@emotion/react';
-import { useLocation, useHistory } from 'react-router-dom';
-import { PrimaryButton, SecondaryButton } from 'components/Button/Button';
+import { useLocation } from 'react-router-dom';
 import { SUGGESTED_REQUEST_DATA } from 'data/request';
 import { POPULAR_REQUEST_DATA } from 'data/helper';
 import { HelpMenu } from 'components/Menu/const';
 import { RequestInfoContent } from 'components/Info/RequestInfoContent';
 import { ProvideInfoContent } from 'components/Info/ProvideInfoContent';
 
-const RequestImage = styled.img`
-  width: 420px;
-  height: 510px;
-  background: yellow;
-  margin-bottom: 20px;
-`;
-
-const RequestCategoryButton = styled(PrimaryButton)`
-  width: max-content;
-  min-width: 140px;
-  padding: 10px 15px;
-  height: 40px;
-  margin: 10px 8px 10px 0px;
-`;
-
-const RequestHashtagButton = styled(SecondaryButton)`
-  width: max-content;
-  min-width: 80px;
-  padding: 10px 15px;
-  height: 40px;
-  margin: 10px 8px 10px 0px;
-`;
-
-const RequestInfoContainer = styled.div`
-  display: grid;
-  grid-template-columns: 180px 400px;
-  grid-gap: 40px;
-  margin-bottom: 60px;
-`;
-
-const RequestDetail = styled.div`
-  font-weight: 700;
-  font-size: 24px;
-  line-height: 28px;
-  color: #000000;
-  min-width: 200px;
-`;
-
-const RequestTitle = styled.div`
-  font-size: 14px;
-  line-height: 26px;
-  color: #cacaca;
-  min-width: 90px;
-  max-width: 150px;
-`;
-
 export const InfoPage = () => {
-  const history = useHistory();
-  const { pathname, state } = useLocation();
-  const query = pathname.split('/')[2];
+  const { state } = useLocation();
   const currentMenu = ((state as any)?.type || HelpMenu.PROVIDE) as HelpMenu;
 
   switch (currentMenu) {
--- src/pages/SearchResultPage.tsx
+++ src/pages/SearchResultPage.tsx
@@ -12,65 +12,10 @@ import { MenuTab } from 'components/Menu/MenuTab';
 import { Divider } from 'components/Divider/Divider';
 import Flex from 'components/Flex/Flex';
 import { PostRequestButton } from 'components/Button/PostRequestButton';
-import { useHistory } from 'react-router-dom';
 import { POPULAR_REQUEST_DATA } from 'data/helper';
 import { SUGGESTED_REQUEST_DATA } from 'data/request';
 import { SuggestedRequestSection } from 'components/Card/SuggestedRequestCard';
 import { PopularRequestSection } from 'components/Card/PopularRequestCard';
-import { SuggestedBadge, RankingBadge } from 'components/Badge/Badge';
-
-const CardContainer = styled.div`
-  width: 95%;
-  height: 341px;
-  background: #ffffff;
-  box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.1);
-  border-radius: 12px;
-  padding: 40px 30px;
-  box-sizing: border-box;
-  position: relative;
-  min-width: 448px;
-  margin-right: 20px;
-  position: relative;
-  top: -20px;
-  margin-top: 40px;
-  cursor: pointer;
-  left: 8px;
-`;
-
-const RequestTitle = styled.div`
-  font-weight: 800;
-  font-size: 24px;
-  margin-bottom: 10px;
-`;
-
-const HelperImage = styled.div`
-  width: 120px;
-  height: 120px;
-  border-radius: 50%;
-  background: #0f3276;
-  margin-top: 15px;
-`;
-
-const RequestDataTitle = styled.div`
-  font-size: 12px;
-  line-height: 16px;
-  color: #c4c4c4;
-  max-width: 91px;
-  margin-right: 15px;
-  width: 80px;
-`;
-
-const RequestDataInfo = styled.div`
-  font-size: 18px;
-  line-height: 26px;
-  color: #000000;
-`;
-
-const RequestDataContent = styled.div`
-  display: flex;
-  align-items: center;
-  margin-bottom: 5px;
-`;
 
 const SearchResultContent = styled.div`
   display: grid;
@@ -80,26 +25,8 @@ const SearchResultContent = styled.div`
   position: relative;
 `;
 
-const SearchResultCard = styled.div`
-  width: 448px;
-  height: 341px;
-  background: #ffffff;
-  box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.1);
-  border-radius: 12px;
-  padding: 40px 30px;
-  box-sizing: border-box;
-  position: relative;
-  min-width: 448px;
-  margin-right: 20px;
-  position: relative;
-  top: -20px;
-  margin-top: 40px;
-  cursor: pointer;
-`;
-
 export const SearchResultPage = () => {
   const [menu, setMenu] = useState<HelpMenu>(HelpMenu.PROVIDE);
-  const history = useHistory();
   const { state } = useLocation();
   const currentMenu = ((state as any)?.menu || HelpMenu.PROVIDE) as HelpMenu;
 
