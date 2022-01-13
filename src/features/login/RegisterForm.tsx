4e41ad441cf27fd538b067d508b50ce0536057de
nnaboon <srisawasdina@gmail.com>
2022-01-14 00:49:45 +0700

    feat: integrate register step

--- src/features/login/RegisterForm.tsx
+++ src/features/login/RegisterForm.tsx
@@ -5,79 +5,119 @@ import { RegisterAccountForm } from './RegisterAccountForm';
 import { RegisterUsernameForm } from './RegisterUsernameForm';
 import { RegisterLocationForm } from './RegisterLocationForm';
 import { RegisterAbilityForm } from './RegisterAbilityForm';
+import { getAuth } from 'firebase/auth';
 import axios from 'axios';
+import { observer } from 'mobx-react-lite';
+import { userStore } from 'store/userStore';
 
 interface RegisterFormProps {
   setProcessStep: (step: LoginStep) => void;
   setIsModalVisible: (isModalVisible: boolean) => void;
 }
 
-export const RegisterForm = ({
-  setProcessStep,
-  setIsModalVisible
-}: RegisterFormProps) => {
-  const [step, setStep] = useState<RegisterStep>(
-    RegisterStep.EMAIL_AND_PASSWORD
-  );
-  const [createUserData, setCreateUserData] = useState<UserCreateBody>();
+export const RegisterForm = observer(
+  ({ setProcessStep, setIsModalVisible }: RegisterFormProps) => {
+    const auth = getAuth();
+    const user = auth.currentUser;
+    const [step, setStep] = useState<RegisterStep>(
+      window.localStorage.getItem('id')
+        ? RegisterStep.LOCATION
+        : user
+        ? RegisterStep.USERNAME
+        : RegisterStep.EMAIL_AND_PASSWORD
+    );
+    const [createUserData, setCreateUserData] = useState<UserCreateBody>();
+    const { setUserId } = userStore;
 
-  const renderForm = (step: RegisterStep) => {
-    switch (step) {
-      case RegisterStep.EMAIL_AND_PASSWORD:
-        return (
-          <RegisterAccountForm
-            userAccountData={createUserData}
-            setProcessStep={setProcessStep}
-            onNext={async (userAccountData) => {
-              await setCreateUserData(userAccountData);
-              setStep(RegisterStep.USERNAME);
-            }}
-          />
-        );
-      case RegisterStep.USERNAME:
-        return (
-          <RegisterUsernameForm
-            userAccountData={createUserData}
-            onNext={async (userAccountData: UserCreateBody) => {
-              await setCreateUserData(userAccountData);
-              setStep(RegisterStep.LOCATION);
-            }}
-          />
-        );
-      case RegisterStep.LOCATION:
-        return (
-          <RegisterLocationForm
-            userAccountData={createUserData}
-            onNext={async (userAccountData: UserCreateBody) => {
-              await setCreateUserData(userAccountData);
-              setStep(RegisterStep.ABILITY);
-            }}
-            onBack={() => {
-              setStep(RegisterStep.USERNAME);
-            }}
-          />
-        );
-      case RegisterStep.ABILITY:
-        return (
-          <RegisterAbilityForm
-            userAccountData={createUserData}
-            onNext={async (userAccountData: UserCreateBody) => {
-              await setCreateUserData(userAccountData);
-              axios
-                .post('http://localhost:5000/users', userAccountData)
-                .then((res) => {
-                  setIsModalVisible(false);
-                })
-                .catch((error) => {
-                  console.log(error);
-                });
-            }}
-            onBack={() => {
-              setStep(RegisterStep.LOCATION);
-            }}
-          />
-        );
-    }
-  };
-  return <div>{renderForm(step)}</div>;
-};
+    useEffect(() => {
+      if (window.localStorage.getItem('id')) {
+        setCreateUserData((prev) => ({
+          ...prev,
+          userId: user.uid,
+          email: user.email,
+          username: user.displayName
+        }));
+      } else if (user) {
+        setCreateUserData((prev) => ({
+          ...prev,
+          userId: user.uid,
+          email: user.email
+        }));
+      }
+    }, []);
+
+    useEffect(() => {
+      if (user?.emailVerified) {
+        setStep(RegisterStep.USERNAME);
+      }
+    }, [user]);
+
+    const renderForm = (step: RegisterStep) => {
+      switch (step) {
+        case RegisterStep.EMAIL_AND_PASSWORD:
+          return (
+            <RegisterAccountForm
+              userAccountData={createUserData}
+              setProcessStep={setProcessStep}
+              onNext={async (userAccountData) => {
+                await setCreateUserData(userAccountData);
+                // axios.post('http://localhost:5000/user/create', {
+                //   email: userAccountData.email,
+                //   password: userAccountData?.password
+                // });
+              }}
+            />
+          );
+        case RegisterStep.USERNAME:
+          return (
+            <RegisterUsernameForm
+              userAccountData={createUserData}
+              onNext={async (userAccountData: UserCreateBody) => {
+                await setCreateUserData(userAccountData);
+                setStep(RegisterStep.LOCATION);
+              }}
+            />
+          );
+        case RegisterStep.LOCATION:
+          return (
+            <RegisterLocationForm
+              userAccountData={createUserData}
+              onNext={async (userAccountData: UserCreateBody) => {
+                await setCreateUserData(userAccountData);
+                setStep(RegisterStep.ABILITY);
+              }}
+              onBack={() => {
+                setStep(RegisterStep.USERNAME);
+              }}
+            />
+          );
+        case RegisterStep.ABILITY:
+          return (
+            <RegisterAbilityForm
+              userAccountData={createUserData}
+              onNext={async (userAccountData: UserCreateBody) => {
+                await setCreateUserData(userAccountData);
+                if (user?.emailVerified) {
+                  console.log(userAccountData);
+
+                  axios
+                    .post('http://localhost:5000/user', userAccountData)
+                    .then((res) => {
+                      setIsModalVisible(false);
+                      setUserId(user.uid);
+                    })
+                    .catch((error) => {
+                      console.log(error);
+                    });
+                }
+              }}
+              onBack={() => {
+                setStep(RegisterStep.LOCATION);
+              }}
+            />
+          );
+      }
+    };
+    return <div>{renderForm(step)}</div>;
+  }
+);
