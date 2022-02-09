317fe04f86d04695553b45093e5982875b218846
nnaboon <srisawasdina@gmail.com>
2022-02-09 17:31:17 +0700

    fix: mobx refact code

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -1,27 +1,34 @@
-import { action, computed, observable } from 'mobx';
+import { action, computed, observable, makeObservable } from 'mobx';
 import { UsersGetDto } from 'models/UserGetDto';
 
 class UserStore {
-  @observable
-  userId: string;
-
-  @observable
+  userId: string = undefined;
+  loginType: string = undefined;
   me: UsersGetDto;
 
-  @action
+  constructor() {
+    makeObservable(this, {
+      userId: observable,
+      me: observable,
+      loginType: observable,
+      setMe: action,
+      setUserId: action,
+      setLoginType: action
+    });
+  }
+
   setMe = (user: UsersGetDto) => {
     this.me = user;
   };
 
-  @computed
-  get isLoggedIn(): boolean {
-    return !!this.userId;
-  }
-
-  @action
   setUserId = (newUserId: string) => {
     this.userId = newUserId;
     window.localStorage.setItem('id', newUserId);
+  };
+
+  setLoginType = (type: string) => {
+    this.loginType = type;
+    window.localStorage.setItem('loginType', type);
   };
 }
 
