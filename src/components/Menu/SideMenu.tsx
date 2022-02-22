519cf2c649a156d30f2e0f3353c00d52bde4e44a
nnaboon <srisawasdina@gmail.com>
2022-02-23 01:31:33 +0700

    fix: path logic in sideMenu

--- src/components/Menu/SideMenu.tsx
+++ src/components/Menu/SideMenu.tsx
@@ -3,6 +3,7 @@
 import { css, jsx } from '@emotion/react';
 import { useHistory } from 'react-router-dom';
 import React from 'react';
+import { observer } from 'mobx-react-lite';
 import { Menu } from 'antd';
 import {
   LogoutOutlined,
@@ -13,6 +14,7 @@ import {
   FileSearchOutlined,
   FileTextOutlined
 } from '@ant-design/icons';
+import { userStore } from 'store/userStore';
 import { CATEGORY } from 'data/category';
 import { MenuSvg } from 'components/Svg/MenuSvg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -24,182 +26,190 @@ interface SideMenuProps {
   setCollapsed: (collapsed: boolean) => void;
 }
 
-export const SideMenu = ({ collapsed, setCollapsed }: SideMenuProps) => {
-  const { SubMenu } = Menu;
-  const history = useHistory();
+export const SideMenu = observer(
+  ({ collapsed, setCollapsed }: SideMenuProps) => {
+    const { SubMenu } = Menu;
+    const history = useHistory();
 
-  const toggleCollapsed = () => {
-    setCollapsed(!collapsed);
-  };
+    const { me } = userStore;
 
-  return (
-    <div
-      style={{
-        height: '100%',
-        width: '100%',
-        position: 'absolute',
-        zIndex: 5
-      }}
-      css={css`
-        .ant-tooltip
-          .ant-menu-inline-collapsed-tooltip
-          .ant-tooltip-placement-left
-          .ant-tooltip-hidden {
-          visibility: hidden !important;
-        }
+    const toggleCollapsed = () => {
+      setCollapsed(!collapsed);
+    };
 
-        .ant-tooltip-inner {
-          visibility: hidden !important;
-        }
-      `}
-    >
+    return (
       <div
-        onClick={toggleCollapsed}
         style={{
-          marginTop: 32,
-          marginLeft: 20,
-          marginBottom: 15,
-          width: '50px'
+          height: '100%',
+          width: '100%',
+          position: 'absolute',
+          zIndex: 5
         }}
         css={css`
-          ${mediaQuerySmallTablet} {
-            position: relative;
-            top: 35px;
+          .ant-tooltip
+            .ant-menu-inline-collapsed-tooltip
+            .ant-tooltip-placement-left
+            .ant-tooltip-hidden {
+            visibility: hidden !important;
           }
 
-          ${mediaQueryMobile} {
-            top: 0;
+          .ant-tooltip-inner {
+            visibility: hidden !important;
           }
         `}
       >
-        <MenuSvg />
-      </div>
-      <Menu
-        defaultSelectedKeys={['1']}
-        mode="inline"
-        theme="dark"
-        inlineCollapsed={collapsed}
-        style={{
-          visibility: !collapsed ? 'visible' : 'hidden',
-          overflowY: 'scroll',
-          width: '100%'
-        }}
-        css={css`
-          height: 100vh;
-          ${mediaQuerySmallTablet} {
-            height: 89vh;
-            position: relative;
-            top: 92px;
-          }
-
-          ${mediaQueryMobile} {
-            height: 92vh;
-            top: 0;
-          }
-        `}
-      >
-        <Menu.Item
-          key="1"
-          icon={<HomeOutlined />}
-          onClick={() => {
-            setCollapsed(true);
-            history.push({
-              pathname: '/'
-            });
+        <div
+          onClick={toggleCollapsed}
+          style={{
+            marginTop: 32,
+            marginLeft: 20,
+            marginBottom: 15,
+            width: '50px'
           }}
+          css={css`
+            ${mediaQuerySmallTablet} {
+              position: relative;
+              top: 35px;
+            }
+
+            ${mediaQueryMobile} {
+              top: 0;
+            }
+          `}
         >
-          หน้าแรก
-        </Menu.Item>
-        <SubMenu key="sub1" icon={<UnorderedListOutlined />} title="หมวดหมู่">
-          {CATEGORY.map(({ id, name, icon }) => (
-            <Menu.Item
-              key={id}
-              onClick={() => {
-                setCollapsed(true);
-                history.push({
-                  pathname: `/${id}`
-                });
-              }}
-              css={css`
-                span {
-                  display: flex;
-                  align-items: center;
-                }
-              `}
-            >
-              <FontAwesomeIcon icon={icon} />
-              <div
+          <MenuSvg />
+        </div>
+        <Menu
+          defaultSelectedKeys={['1']}
+          mode="inline"
+          theme="dark"
+          inlineCollapsed={collapsed}
+          style={{
+            visibility: !collapsed ? 'visible' : 'hidden',
+            overflowY: 'scroll',
+            width: '100%'
+          }}
+          css={css`
+            height: 100vh;
+            ${mediaQuerySmallTablet} {
+              height: 89vh;
+              position: relative;
+              top: 92px;
+            }
+
+            ${mediaQueryMobile} {
+              height: 92vh;
+              top: 0;
+            }
+          `}
+        >
+          <Menu.Item
+            key="1"
+            icon={<HomeOutlined />}
+            onClick={() => {
+              setCollapsed(true);
+              history.push({
+                pathname: '/'
+              });
+            }}
+          >
+            หน้าแรก
+          </Menu.Item>
+          <SubMenu key="sub1" icon={<UnorderedListOutlined />} title="หมวดหมู่">
+            {CATEGORY.map(({ id, name, icon }) => (
+              <Menu.Item
+                key={id}
+                onClick={() => {
+                  setCollapsed(true);
+                  history.push({
+                    pathname: `/${id}`
+                  });
+                }}
                 css={css`
-                  margin-left: 10px;
+                  span {
+                    display: flex;
+                    align-items: center;
+                  }
                 `}
               >
-                {name}
-              </div>
-            </Menu.Item>
-          ))}
-        </SubMenu>{' '}
-        <Menu.Item
-          key="2"
-          icon={<MessageOutlined />}
-          onClick={() => {
-            setCollapsed(true);
-            history.push({
-              pathname: '/chat'
-            });
-          }}
-        >
-          กล่องข้อความ
-        </Menu.Item>
-        <Menu.Item
-          key="3"
-          icon={<UsergroupAddOutlined />}
-          onClick={() => {
-            setCollapsed(true);
-            history.push({
-              pathname: `/community/${
-                window.localStorage.getItem('selectedCommunity') ?? null
-              }`
-            });
-          }}
-        >
-          ชุมชนความช่วยเหลือ
-        </Menu.Item>
-        <Menu.Item
-          key="4"
-          icon={<FileSearchOutlined />}
-          onClick={() => {
-            setCollapsed(true);
-            history.push({
-              pathname: '/order/request'
-            });
-          }}
-        >
-          รายการขอความช่วยเหลือของฉัน
-        </Menu.Item>
-        <Menu.Item
-          key="9"
-          icon={<FileTextOutlined />}
-          onClick={() => {
-            setCollapsed(true);
-            history.push({
-              pathname: '/order/provide'
-            });
-          }}
-        >
-          รายการให้ความช่วยเหลือของฉัน
-        </Menu.Item>
-        <Menu.Item
-          key="10"
-          icon={<LogoutOutlined />}
-          onClick={() => {
-            window.localStorage.removeItem('id');
-            auth.signOut();
-            window.location.assign('/');
-          }}
-        >
-          ออกจากระบบ
-        </Menu.Item>
-      </Menu>
-    </div>
-  );
-};
+                <FontAwesomeIcon icon={icon} />
+                <div
+                  css={css`
+                    margin-left: 10px;
+                  `}
+                >
+                  {name}
+                </div>
+              </Menu.Item>
+            ))}
+          </SubMenu>{' '}
+          <Menu.Item
+            key="2"
+            icon={<MessageOutlined />}
+            onClick={() => {
+              setCollapsed(true);
+              history.push({
+                pathname: '/chat'
+              });
+            }}
+          >
+            กล่องข้อความ
+          </Menu.Item>
+          <Menu.Item
+            key="3"
+            icon={<UsergroupAddOutlined />}
+            onClick={() => {
+              setCollapsed(true);
+              history.push({
+                pathname: window.localStorage.getItem('seletedCommunity')
+                  ? `/community/${window.localStorage.getItem(
+                      'selectedCommunity'
+                    )}`
+                  : me?.communityId
+                  ? `/community/${me?.communityId[0]}`
+                  : '/community'
+              });
+            }}
+          >
+            ชุมชนความช่วยเหลือ
+          </Menu.Item>
+          <Menu.Item
+            key="4"
+            icon={<FileSearchOutlined />}
+            onClick={() => {
+              setCollapsed(true);
+              history.push({
+                pathname: '/order/request'
+              });
+            }}
+          >
+            รายการขอความช่วยเหลือของฉัน
+          </Menu.Item>
+          <Menu.Item
+            key="9"
+            icon={<FileTextOutlined />}
+            onClick={() => {
+              setCollapsed(true);
+              history.push({
+                pathname: '/order/provide'
+              });
+            }}
+          >
+            รายการให้ความช่วยเหลือของฉัน
+          </Menu.Item>
+          <Menu.Item
+            key="10"
+            icon={<LogoutOutlined />}
+            onClick={() => {
+              window.localStorage.removeItem('id');
+              auth.signOut();
+              window.location.assign('/');
+            }}
+          >
+            ออกจากระบบ
+          </Menu.Item>
+        </Menu>
+      </div>
+    );
+  }
+);
