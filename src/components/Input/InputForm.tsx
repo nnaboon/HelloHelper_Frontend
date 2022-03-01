d77caf921277f966974572fb66a6eadd65ec9631
nnaboon <srisawasdina@gmail.com>
2022-03-01 17:47:23 +0700

    fix: uncomment line-height in input form

--- src/components/Input/InputForm.tsx
+++ src/components/Input/InputForm.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from '@emotion/styled';
 import { Input } from 'antd';
 import { mediaQueryLargeDesktop, mediaQueryMobile } from 'styles/variables';
+import { mediaQueryMiniDesktop } from '../../styles/variables';
 
 export const InputForm = styled(Input)`
   height: 40px;
@@ -9,7 +10,7 @@ export const InputForm = styled(Input)`
   max-height: 40px;
   font-size: 16px;
   border-radius: 12px;
-  // line-height: 6.8713;
+  line-height: 6.8713;
 
   ${mediaQueryLargeDesktop} {
     height: 35px;
@@ -18,17 +19,24 @@ export const InputForm = styled(Input)`
     border-radius: 10px;
     font-size: 14px;
   }
+
+  ${mediaQueryMiniDesktop} {
+    line-height: 1.8713;
+  }
 `;
 
 export const InputPasswordForm = styled(Input.Password)`
   height: 40px;
   font-size: 16px;
   border-radius: 12px;
-  // line-height: 6.8713;
 
   ${mediaQueryLargeDesktop} {
     height: 35px;
     border-radius: 10px;
     font-size: 14px;
+  }
+
+  ${mediaQueryMiniDesktop} {
+    line-height: 1.8713;
   }
 `;
